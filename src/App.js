6215f20d370fa515863ebe0ab6bd4b7451a53b4c
vithlanibhakti <vithlanibhakti18@gmail.com>
2020-05-19 18:05:51 +0530

    rect js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import FormValidator from './FormValidator';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <FormValidator />
+    );
+  }
+}
+
+export default App;
